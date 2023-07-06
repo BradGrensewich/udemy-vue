c29a3c5a745c77154776a421a966612a0843adec
BradGrensewich <bradgrensewich@gmail.com>
2023-07-06 11:49:30 +0700

    basic interpolation and attribute binding

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      goal: "Learn reactive frontend design",
+      goal: 'Learn reactive frontend design',
+      vueLink: 'https://vuejs.org'
     };
-  },
-})
+  }
+});
 
 app.mount('#user-goal')
