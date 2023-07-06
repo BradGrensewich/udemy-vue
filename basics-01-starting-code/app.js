df98b8d52c285ebbdb8f83771a92859cf88d647e
BradGrensewich <bradgrensewich@gmail.com>
2023-07-06 12:43:59 +0700

    working with data inside the Vue app object

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      goal: 'Learn reactive frontend design',
+      goalA: 'Learn reactive frontend design',
+      goalB: 'Master Vue',
       vueLink: 'https://vuejs.org'
     };
   },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
     outputGoal () {
         const randomNumber = Math.random();
         if (randomNumber < 0.5) {
-            return 'Learn Vue';
+            return this.goalA;
         } else {
-            return 'Master Vue';
+            return this.goalB;
         }
     }
   }
