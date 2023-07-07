e02d1a44f1384d758164a2f52a65ee2e96cf8119
BradGrensewich <bradgrensewich@gmail.com>
2023-07-07 14:30:47 +0700

    the native event object

--- lecs2/app.js
+++ lecs2/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(e, lastName) {
+      this.name = e.target.value + ' ' + lastName;
+    },
     countUp(num) {
       this.counter += num;
     },
