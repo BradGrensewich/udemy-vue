baa794db42fa295e9742a8492239621995b81f52
BradGrensewich <bradgrensewich@gmail.com>
2023-07-07 14:53:09 +0700

    basic event modifiers

--- lecs2/app.js
+++ lecs2/app.js
@@ -2,20 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("submitted");
+    },
     setName(e, lastName) {
-      this.name = e.target.value + ' ' + lastName;
+      this.name = e.target.value + " " + lastName;
     },
     countUp(num) {
       this.counter += num;
     },
     countDown(num) {
       this.counter -= num;
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
