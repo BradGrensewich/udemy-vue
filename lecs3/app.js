b11abd75ca9a2d7ef52f17e632a7b4ca030cd08e
BradGrensewich <bradgrensewich@gmail.com>
2023-07-08 12:06:50 +0700

    watchers

--- lecs3/app.js
+++ lecs3/app.js
@@ -3,12 +3,34 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      //fullName: ''
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 100) {
+        this.counter = 0;
+      }
+    },
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = ''
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = ''
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // }
   },
   computed: {
     fullName() {
-      console.log("running again");
-      return this.name + " " + "Grensewich";
+      return this.name + " " + this.lastName;
     },
   },
   methods: {
