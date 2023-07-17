28b24d91c0fba61a81b5415615c44feff107a183
BradGrensewich <bradgrensewich@gmail.com>
2023-07-17 18:47:43 +0700

    a vue app skeleton w the vue cli

---
+++ basic-view-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'brad',
+                    name: 'Brad Grensewich',
+                    phone: '0702519921',
+                    email: 'brad@localhost.com'
+                },
+                {
+                    id: 'vy',
+                    name: 'Vy Ngo',
+                    phone: '911',
+                    email: 'vy@localhost.com'
+                }
+            ]
+        }        
+    }
+};
+</script>
+<style></style>
