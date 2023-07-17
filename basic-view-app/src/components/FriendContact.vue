180b58f66481bb69b11dc0d892d92df79528ed1f
BradGrensewich <bradgrensewich@gmail.com>
2023-07-17 19:08:16 +0700

    adding a component in vue cli

---
+++ basic-view-app/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+	<li>
+		<h2>{{ friend.name }}</h2>
+		<button @click="toggleDetails">Show Details</button>
+		<ul v-if="detailsAreVisible">
+			<li><strong>Phone:</strong>{{ friend.phone }}</li>
+			<li><strong>Email:</strong>{{ friend.email }}</li>
+		</ul>
+	</li>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			detailsAreVisible: false,
+			friend: {
+				id: 'brad',
+				name: 'Brad Grensewich',
+				phone: '0702519921',
+				email: 'brad@localhost.com',
+			},
+		};
+	},
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
+
+<style></style>
