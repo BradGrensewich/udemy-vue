212c5e745e9a392f05f074f2c086d7b75554a743
BradGrensewich <bradgrensewich@gmail.com>
2023-07-23 12:48:43 +0700

    registering components locally + scoped styles

---
+++ adv-components/application/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Brad Grensewich',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
---
+++ adv-components/application/src/components/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </header>
+    <p>{{ infoText }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
