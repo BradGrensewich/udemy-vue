01ff640efd0b50481003afe4dcb069e8a2c16296
BradGrensewich <bradgrensewich@gmail.com>
2023-07-25 17:38:28 +0700

    dynamic components

--- adv-components/application/src/App.vue
+++ adv-components/application/src/App.vue
@@ -1,36 +1,34 @@
 <template>
 	<div>
 		<TheHeader />
-		<BadgeList />
-		<UserInfo
-			:full-name="activeUser.name"
-			:info-text="activeUser.description"
-			:role="activeUser.role"
-		/>
-    <CourseGoals>
-      <template #default="exampleObject">
-        <h3>{{ exampleObject.item }}</h3>
-        <p> {{ exampleObject.anotherItem }}</p>
-      </template>
-    </CourseGoals>
+    <button @click="setSelectedComponent('ActiveGoals')">ActiveGoals</button>
+    <button @click="setSelectedComponent('ManageGoals')">ManageGoals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'ActiveGoals'"/>
+    <ManageGoals v-if="selectedComponent === 'ManageGoals'"/> -->
+    <component :is="selectedComponent"></component>
 	</div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
 	components: {
 		TheHeader,
-		BadgeList,
-		UserInfo,
-    CourseGoals
+		// BadgeList,
+		// UserInfo,
+		// CourseGoals,
+		ActiveGoals,
+		ManageGoals,
 	},
 	data() {
 		return {
+      selectedComponent: 'ActiveGoals',
 			activeUser: {
 				name: 'Brad Grensewich',
 				description: 'Site owner and admin',
@@ -38,6 +36,11 @@ export default {
 			},
 		};
 	},
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
