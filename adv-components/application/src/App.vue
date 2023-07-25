4bd9701e1585fd4b080cfae5674204066e589c33
BradGrensewich <bradgrensewich@gmail.com>
2023-07-25 17:01:25 +0700

    scoped slots

--- adv-components/application/src/App.vue
+++ adv-components/application/src/App.vue
@@ -7,6 +7,12 @@
 			:info-text="activeUser.description"
 			:role="activeUser.role"
 		/>
+    <CourseGoals>
+      <template #default="exampleObject">
+        <h3>{{ exampleObject.item }}</h3>
+        <p> {{ exampleObject.anotherItem }}</p>
+      </template>
+    </CourseGoals>
 	</div>
 </template>
 
@@ -14,12 +20,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
 	components: {
 		TheHeader,
 		BadgeList,
 		UserInfo,
+    CourseGoals
 	},
 	data() {
 		return {
---
+++ adv-components/application/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-item="This is just to demo accessing scoped slots kebab-case"></slot>    
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Learn Vue', 'Finish the course']
+        }
+    }
+}
+</script>
