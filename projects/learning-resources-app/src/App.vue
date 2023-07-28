1a26074bf1fbe3a1c7e09e03383ecbcac7aa7501
BradGrensewich <bradgrensewich@gmail.com>
2023-07-28 13:32:06 +0700

    added basic styles and stored resources component

--- projects/learning-resources-app/src/App.vue
+++ projects/learning-resources-app/src/App.vue
@@ -1,18 +1,12 @@
 <template>
-	<ul>
-		<LearningResource
-			v-for="res in storedResources"
-			:key="res.id"
-			:="res"
-		/>
-	</ul>
+	<StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
-	components: { LearningResource },
+	components: { LearningResource, StoredResources },
 	data() {
 		return {
 			storedResources: [
@@ -33,3 +27,19 @@ export default {
 	},
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+	box-sizing: border-box;
+}
+
+html {
+	font-family: 'Roboto', sans-serif;
+}
+
+body {
+	margin: 0;
+}
+</style>
