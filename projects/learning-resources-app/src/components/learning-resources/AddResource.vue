e12a02c9b223cc9dea28674fdfce3b929b17590b
BradGrensewich <bradgrensewich@gmail.com>
2023-07-28 15:02:10 +0700

    form skeleton added to AddResource component

--- projects/learning-resources-app/src/components/learning-resources/AddResource.vue
+++ projects/learning-resources-app/src/components/learning-resources/AddResource.vue
@@ -1,3 +1,53 @@
 <template>
-    <h2>Add Resource</h2>
+	<BaseCard>
+		<form>
+			<div class="form-control">
+				<label for="title">Title</label>
+				<input type="text" id="title" name="title" />
+			</div>
+            <div class="form-control">
+				<label for="description">Description</label>
+				<textarea id="description" name="description" rows="3"></textarea>
+			</div>
+            <div class="form-control">
+				<label for="link">Link</label>
+				<input type="url" id="link" name="link" />
+			</div>
+            <div>
+                <BaseButton type="submit">Add Resource</BaseButton>
+            </div>
+		</form>
+	</BaseCard>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
