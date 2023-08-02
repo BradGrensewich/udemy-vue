54b17a8605874216f7fa8c0e1f7c62a3bd80b329
BradGrensewich <bradgrensewich@gmail.com>
2023-08-02 18:17:55 +0700

    retrieving data with GET using fetch(), then(response), and then(data)

--- http-requests/src/components/survey/UserExperiences.vue
+++ http-requests/src/components/survey/UserExperiences.vue
@@ -1,37 +1,66 @@
 <template>
-  <section>
-    <base-card>
-      <h2>Submitted Experiences</h2>
-      <div>
-        <base-button>Load Submitted Experiences</base-button>
-      </div>
-      <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
-      </ul>
-    </base-card>
-  </section>
+	<section>
+		<base-card>
+			<h2>Submitted Experiences</h2>
+			<div>
+				<base-button @click="loadSubmittedExperiences"
+					>Load Submitted Experiences</base-button
+				>
+			</div>
+			<ul>
+				<survey-result
+					v-for="result in results"
+					:key="result.id"
+					:name="result.name"
+					:rating="result.rating"
+				></survey-result>
+			</ul>
+		</base-card>
+	</section>
 </template>
 
 <script>
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
-  components: {
-    SurveyResult,
-  },
+	data() {
+		return {
+			results: [],
+		};
+	},
+	components: {
+		SurveyResult,
+	},
+	methods: {
+		loadSubmittedExperiences() {
+			fetch(
+				'https://vue-http-demo-4567d-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+			)
+				.then((response) => {
+					if (response.ok) {
+						return response.json();
+					}
+				})
+				.then((data) => {
+					const results = [];
+					for (const id in data) {
+						results.push({
+							id: id,
+							name: data[id].name,
+							rating: data[id].rating,
+						});
+					}
+					this.results = results;
+				});
+		},
+	},
 };
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 }
 </style>
