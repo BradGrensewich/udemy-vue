4600e72b1664f69856343cf2eda15004ea50bbb7
BradGrensewich <bradgrensewich@gmail.com>
2023-08-14 20:35:01 +0700

    connecting multiple components to state

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <base-container title="Vuex">
-  <h3> {{ $store.state.counter }}</h3>
-  <button>Add 1</button>
-  </base-container>
+	<base-container title="Vuex">
+		<TheCounter />
+		<button @click="addOne">Add 1</button>
+	</base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
-  components: {
-    BaseContainer,
-  },
+	components: {
+		BaseContainer,
+		TheCounter,
+	},
+	methods: {
+		addOne() {
+			this.$store.state.counter++;
+		},
+	},
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 html {
-  font-family: sans-serif;
+	font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
