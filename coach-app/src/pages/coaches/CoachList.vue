21fc746e3b347e190c386e4be4c8d4885d78515c
BradGrensewich <bradgrensewich@gmail.com>
2023-08-23 14:47:23 +0700

    added coach filter component and integrated into coach-list

--- coach-app/src/pages/coaches/CoachList.vue
+++ coach-app/src/pages/coaches/CoachList.vue
@@ -1,14 +1,18 @@
 <template>
-	<section>FILTER</section>
+	<section>
+		<CoachFilter @change-filters="setFilters"></CoachFilter>
+	</section>
 	<section>
 		<BaseCard>
 			<div class="controls">
 				<BaseButton mode="outline">Refresh</BaseButton>
-				<BaseButton link :to="{ name: 'register' }">Register as Coach</BaseButton>
+				<BaseButton link :to="{ name: 'register' }"
+					>Register as Coach</BaseButton
+				>
 			</div>
 			<ul v-if="hasCoaches">
 				<CoachItem
-					v-for="coach in coaches"
+					v-for="coach in filteredCoaches"
 					:key="coach.id"
 					:id="coach.id"
 					:first-name="coach.firstName"
@@ -25,17 +29,44 @@
 <script>
 import { mapGetters } from 'vuex';
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from './CoachFilter.vue';
 
 export default {
+	data() {
+		return {
+			activeFilters: {
+				frontend: true,
+				backend: true,
+				career: true,
+				graphicDesign: true,
+			},
+		};
+	},
 	computed: {
 		...mapGetters(['coaches', 'hasCoaches']),
+		filteredCoaches() {
+			return this.coaches.filter((coach) => {
+				for (const area in this.activeFilters) {
+					if (
+						this.activeFilters[area] &&
+						coach.areas.includes(area)
+					) {
+						return true;
+					}
+				}
+				return false;
+			});
+		},
 	},
 	methods: {
 		refreshCoaches() {
 			console.log('refreshed TODO');
 		},
+		setFilters(updatedFilters) {
+			this.activeFilters = updatedFilters;
+		},
 	},
-	components: { CoachItem },
+	components: { CoachItem, CoachFilter },
 };
 </script>
 
