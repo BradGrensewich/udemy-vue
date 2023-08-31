776343d1d860fcd63cbb491b3bda19481026c0af
BradGrensewich <bradgrensewich@gmail.com>
2023-08-31 18:02:23 +0700

    added notFound.vue

--- coach-app/src/pages/NotFound.vue
+++ coach-app/src/pages/NotFound.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        
-    </div>
+    <section>
+        <BaseCard>
+            <h2>Page Not Found</h2>
+            <p>This page could not be found - maybe check out our <RouterLink to="/coaches">coaches</RouterLink></p>
+        </BaseCard>
+    </section>
 
 </template>
 
