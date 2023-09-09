761f8e8af42d923d4b89791e5fb2ece0fe4efed4
BradGrensewich <bradgrensewich@gmail.com>
2023-09-09 14:29:01 +0700

    composition api computed

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -3,18 +3,33 @@
 		<h2>{{ user.name }}</h2>
 		<h3>{{ user.age }}</h3>
 		<button @click="increaseAge">Increase Age</button>
+		<div>
+			<input type="text" placeholder="First Name" v-model="fname" />
+			<input type="text" placeholder="Last Name" v-model="lname" />
+		</div>
 	</section>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref, computed } from 'vue';
 
-const user = reactive({ name: 'Bradley', age: 30 });
-// const userName = ref('Bradley');
-// const age = ref(30)
+const fname = ref('');
+const lname = ref('');
+const userName = computed(function() {
+	return fname.value + ' ' + lname.value;
+});
+
+const age = ref(30);
 function increaseAge() {
-	user.age++;
+	age.value++;
 }
+
+const user = reactive({
+	name: userName,
+	age: age
+})
+
+
 </script>
 
 <style>
